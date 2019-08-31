1d83ab882c261b9f61edfaf6401ed2f333a3e8db
Saytzat <lsoysy@163.com>
2019-08-31 16:37:55 +0800

    提交文件

--- src/main/webapp/Hello.java
+++ src/main/webapp/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("TEST");
+        System.out.println("git03...");
 
     }
 }
